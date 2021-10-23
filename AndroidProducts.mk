8760261ead892502ae0790eadc951699a084eca9
秋风有萧 <91730567+akong2233@users.noreply.github.com>
2021-10-23 20:07:40 +0800

    initial
    initial

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_trident.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_trident-user \
+    omni_trident-userdebug \
+    omni_trident-eng
